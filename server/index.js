21eb39fd25ff841f0067090e08dc62e110176292
KaedenOC <komeacarr2@gmail.com>
2023-05-31 15:32:02 -0700

    namespace created in server/index.js

--- server/index.js
+++ server/index.js
@@ -7,5 +7,17 @@ const PORT = process.env.PORT || 3002;
 //socket server singleton
 const server = new Server();
 
-//listening for all events on
+//listening for all events
 server.listen(PORT);
+
+//create namespace
+const capsNameSpace = server.of('/caps');
+capsNameSpace.on('connection', (socket) => {
+  console.log('socket connected to namespace', socket.id);
+
+  //join room
+  socket.on('JOIN', (room) => {
+    socket.join(room);
+    console.log(`joined the ${room} room`);
+  });
+});
